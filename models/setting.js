9310fee9c004488245aa519adcfb94ba5cfba682
Pierre Berghof <pberghof@macbook-pro-de-pierre-4.home>
2019-06-27 09:35:52 +0200

    edit

--- models/setting.js
+++ models/setting.js
@@ -2,19 +2,7 @@ const mongoose = require("mongoose");
 
 const settingsSchema = new mongoose.Schema({
   access_token: { type: String },
-  post_id: { type: String },
-  phase: {
-    one: { type: Number },
-    two: { type: Number },
-    three: { type: Number },
-    four: { type: Number },
-    five: { type: Number },
-    six: { type: Number },
-    seven: { type: Number },
-    eight: { type: Number },
-    nine: { type: Number },
-    ten: { type: Number }
-  }
+  post_id: { type: String }
 });
 
 const setting = mongoose.model('Settings', settingsSchema);
